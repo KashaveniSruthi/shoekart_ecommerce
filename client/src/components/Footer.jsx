b67f4b693150fbdf197ffc6337320cb25804be4d
Shruthi Kashaveni <141952842+KashaveniSruthi@users.noreply.github.com>
2025-02-08 19:58:06 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -138,7 +138,7 @@ const Footer = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Sumil Suthar
+                Shruthi Kashaveni
               </Link>
             </p>
           </div>
